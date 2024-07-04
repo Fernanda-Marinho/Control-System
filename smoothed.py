10c424dffef1c22bb82fbad29536a684e532e1ec
Fernanda-Marinho <fmarinho1918@gmail.com>
2024-07-04 01:24:54 -0300

    smoothed and polimonial graphics

---
+++ smoothed.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+data = pd.read_csv('21-06.csv', delimiter=r'\s+')
+
+tempo_col_index = 3
+sensor1_col_index = 6
+
+tempo = data.iloc[:, tempo_col_index]
+sensor1 = data.iloc[:, sensor1_col_index]
+
+def media_movel(data, window_size):
+    return data.rolling(window=window_size).mean()
+
+window_size = 20
+
+sensor1_suave = media_movel(sensor1, window_size)
+
+def polynomial_regression(x, y, d):
+    poly_features = PolynomialFeatures(d=d)
+    x_poly = poly_features.fit_transform(x.reshape(-1, 1))
+    model = LinearRegression()
+    model.fit(x_poly, y)
+    y_poly_pred = model.predict(x_poly)
+    return y_poly_pred
+
+degree = 2 
+tempo_numeric = np.arange(len(tempo))
+sensor1_poly = polynomial_regression(tempo_numeric, sensor1_suave.fillna(0), degree)
+
+plt.figure(figsize=(14, 10))
+plt.plot(tempo, sensor1_poly, label='Sensor 1 (Polinomial)', linestyle='-', linewidth=2)
+plt.xlabel('Tempo')
+plt.ylabel('Temperatura')
+plt.title('Leituras dos Sensores ao longo do Tempo (Polinomial)')
+plt.legend()
+plt.grid(True)
+plt.show()
